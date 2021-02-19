352cfa19bd2e079a5293d2d0847420b78d7a6a83
chris interiano <cinteriano@live.com>
2021-02-18 16:38:48 -0800

    working on javascript

---
+++ Assets/script.js
@@ -0,0 +1,35 @@
+var questions = [
+    {
+        title: "what year did pink floyd form?", 
+        choices: ["1965", "1970" , "1962" , "1971"],
+        answer: "1965"
+
+    },
+    
+    {
+        title: "what year did pink floyd form?", 
+        choices: ["1965", "1970" , "1962" , "1971"],
+        answer: "1965"
+
+    },
+    
+    {
+        title: "what year did pink floyd form?", 
+        choices: ["1965", "1970" , "1962" , "1971"],
+        answer: "1965"
+
+    },
+    
+    {
+        title: "what year did pink floyd form?", 
+        choices: ["1965", "1970" , "1962" , "1971"],
+        answer: "1965"
+
+    },
+        
+        
+];
+
+
+
+
